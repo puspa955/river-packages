898526ab3f46c0c23fbfdcce435bc082276658c3
puspa <limbup503@gmail.com>
2025-11-02 11:24:46 +0545

    minor fix

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,8 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 import postcss from 'rollup-plugin-postcss';
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+
 
 export default {
   input: 'src/index.js',
@@ -34,35 +36,29 @@ export default {
     'react-day-picker',
     'tailwind-merge',
   ],
-  plugins: [
-    // Resolve first to find modules
-    resolve({
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    }),
-    // Transform JSX BEFORE commonjs tries to parse it
-    babel({
-      babelHelpers: 'bundled',
-      exclude: 'node_modules/**',
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      presets: [
-        ['@babel/preset-env', {
-          targets: { node: '14' }
-        }],
-        ['@babel/preset-react', { 
-          runtime: 'automatic' 
-        }]
-      ],
-    }),
-    // Now commonjs can work with the transformed code
-    commonjs({
-      include: /node_modules/,
-    }),
-    postcss({
-      extensions: ['.css'],
-      minimize: true,
-      inject: false,
-      extract: false,
-    }),
-    terser(),
-  ],
+ plugins: [
+  peerDepsExternal(),
+  resolve({
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  }),
+  babel({
+    babelHelpers: "bundled",
+    exclude: "node_modules/**",
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    presets: [
+      ["@babel/preset-env", { targets: { node: "14" } }],
+      ["@babel/preset-react", { runtime: "automatic" }],
+    ],
+  }),
+  commonjs({
+    include: /node_modules/,
+  }),
+  postcss({
+    extensions: [".css"],
+    minimize: true,
+    inject: false,
+    extract: false,
+  }),
+  terser(),
+]
 };
